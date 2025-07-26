b63aaa50b14ad50662cb88edcca60040be74bbfe
Void Drifter <drifter@voltaccept.com>
2025-07-26 17:49:34 -0400

    Add .next/static/chunks/src_app_inventory_page_tsx_a31f8fc9._.js 1753566637043

---
+++ .next/static/chunks/src_app_inventory_page_tsx_a31f8fc9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ce509cc9._.js",
+  "static/chunks/src_78508293._.js"
+],
+    source: "dynamic"
+});
