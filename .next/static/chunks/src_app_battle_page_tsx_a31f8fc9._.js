59458fadac64a5ed4b7d8e8b9dcfa9de420ae604
Void Drifter <drifter@voltaccept.com>
2025-07-26 17:49:29 -0400

    Add .next/static/chunks/src_app_battle_page_tsx_a31f8fc9._.js 1753566632488

---
+++ .next/static/chunks/src_app_battle_page_tsx_a31f8fc9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_3e9a6fe0._.js",
+  "static/chunks/src_be276cf1._.js"
+],
+    source: "dynamic"
+});
